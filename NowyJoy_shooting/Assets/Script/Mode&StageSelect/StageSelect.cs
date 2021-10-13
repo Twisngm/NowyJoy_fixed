e9c12d029e4851cb2630ea2b18ffc5c631ff1b92
DoJiSeon <djs6620@gmail.com>
2021-10-13 23:20:10 +0900

    스테이지 선택 스크립트 수정

---
+++ NowyJoy_shooting/Assets/Script/Mode&StageSelect/StageSelect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelect : MonoBehaviour
+{
+    public GameObject stage1_1;
+    //float checkTime = 0f;
+
+    //[SerializeField] [Range(1f, 5f)] float scaleSpeed = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Stage"))
+        {
+            if (collision.name == "1-1")
+            {
+                Debug.Log("1-1과 닿았습니다.");
+            }
+        }
+    }
