baf19a4b5ac41a06d5ad034417ac168dad1468cf
DoJiSeon <djs6620@gmail.com>
2021-10-19 20:53:56 +0900

    스테이지 선택 창 개발중

--- NowyJoy_shooting/Assets/Script/Mode&StageSelect/StageSelect.cs
+++ NowyJoy_shooting/Assets/Script/Mode&StageSelect/StageSelect.cs
@@ -7,6 +7,7 @@ public class StageSelect : MonoBehaviour
 {
     public string stage;
     
+    [SerializeField] [Range(1f, 5f)] float scaleSpeed = 1f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,6 +18,7 @@ public class StageSelect : MonoBehaviour
                 case "1":
                     Debug.Log("1 스테이지로 이동합니다.");
                     //SceneManager.LoadScene("stage1");
+                    StartCoroutine("changing");
                     break;
                 case "1-1":
                     Debug.Log("1-1 스테이지로 이동합니다.");
@@ -27,4 +29,60 @@ public class StageSelect : MonoBehaviour
             }
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("나갔습니다.");
+        StopCoroutine("changing");
+        StartCoroutine("changing_small");
+    }
+
+
+    void changed()
+    {
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x + 1f * scaleSpeed * Time.deltaTime, gameObject.transform.localScale.y + 1f * scaleSpeed * Time.deltaTime, 0);
+    }
+
+    void changed_small()
+    {
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x - 1f * scaleSpeed * Time.deltaTime, gameObject.transform.localScale.y - 1f * scaleSpeed * Time.deltaTime, 0);
+    }
+
+
+    IEnumerator changing()
+    {
+        float checkTime = 0f;
+        
+        while (checkTime <= 4f)
+        {
+            yield return new WaitForSecondsRealtime(0.02f);
+            checkTime += 0.1f;
+            changed();
+            PlayerPrefs.SetFloat("checktime", checkTime);
+        }
+        
+        if (checkTime > 4f)
+        {
+            yield break;
+        }
+    }
+
+    IEnumerator changing_small()
+    {
+        float checkTime = PlayerPrefs.GetFloat("checktime", 0f);
+        while (checkTime >= 0f && checkTime < 4f)
+        {
+            yield return new WaitForSecondsRealtime(0.02f);
+            checkTime -= 0.1f;
+            changed_small();
+        }
+        if (checkTime < 0f)
+        {
+            yield break;
+        }
+
+
+    }
+
+
 }
