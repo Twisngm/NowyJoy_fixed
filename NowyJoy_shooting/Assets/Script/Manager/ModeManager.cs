7700e7095ea0efa75316df205892dd3248504290
Twisngm <chduaahd24@naver.com>
2021-12-27 21:49:11 +0900

    옵션 버튼 수정

--- NowyJoy_shooting/Assets/Script/Manager/ModeManager.cs
+++ NowyJoy_shooting/Assets/Script/Manager/ModeManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class ModeManager : MonoBehaviour
 {
+    public GameManager GM;
+    public GameObject button1;
+    public GameObject button2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GM = GameManager.GM_Instance;
+        if (GM.Perfectmode)
+        {
+            button1.SetActive(true);
+        }
+        else
+        {
+            button1.SetActive(false);
+        }
+        if (GM.isUnlock)
+        {
+            button2.SetActive(true);
+        }
+        else
+        {
+            button2.SetActive(false);
+        }
     }
 
     // Update is called once per frame
